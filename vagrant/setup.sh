23c304cc2adfb79f9fffc8729b702bdd7026e28a
Rafael Queiroz <rafaelfqf@gmail.com>
2016-10-30 16:24:16 -0300

    Adding tor in setup.sh

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install -y --force-yes apache2 libapache2-mod-php5 php5 php5-mysql php5-mcrypt php5-curl php5-intl
+sudo apt-get install -y --force-yes apache2 tor libapache2-mod-php5 php5 php5-mysql php5-mcrypt php5-curl php5-intl
 
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
